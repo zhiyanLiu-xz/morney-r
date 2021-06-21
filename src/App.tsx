e1ab51aa7e9258e48a4f77f814559177fa84aedb
刘芷言 <liuzhiyan138@163.com>
2021-06-21 14:09:34 +0800

    Money.tsx 之 TagsSection UI 完成

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,16 @@ import Tags from './views/Tags';
 import Money from './views/Money';
 import Statistics from './views/Statistics';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  color: #333;
+`
 
 function App() {
   return (
-    <Router>
+    <AppWrapper>
+      <Router>
           <Switch>
             <Route path="/tags">
               <Tags />
@@ -28,7 +34,8 @@ function App() {
               <NoMatch />
             </Route>
           </Switch>
-    </Router>
+      </Router>
+    </AppWrapper>
   );
 }
 
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import Icon from './Icon';
 
 
@@ -22,7 +22,14 @@ const NavWrapper = styled.nav`
           width: 24px;      
           height: 24px;
         }
+        &.selected{
+            color: red;
+            .icon{
+              fill: red;
+            }
+          }
       }
+    }
   }
 `;
 
@@ -31,22 +38,22 @@ const Nav = ()=>{
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/tags">
+          <NavLink to="/tags" activeClassName="selected">
             <Icon name="tag"/>
             标签页
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/money">
+          <NavLink to="/money" activeClassName="selected">
             <Icon name="money"/>
             记账页
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/statistics">
+          <NavLink to="/statistics" activeClassName="selected">
             <Icon name="chart"/>
             统计页
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
