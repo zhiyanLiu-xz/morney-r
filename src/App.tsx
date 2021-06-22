6bcda8b37876e080e73fec96d2d883979960add0
刘芷言 <liuzhiyan138@163.com>
2021-06-22 19:50:21 +0800

    封装 findTag 到 useTags 里

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
             <Route exact path="/tags">
               <Tags />
             </Route>
-            <Route exact path="/tags/:tag">
+            <Route exact path="/tags/:id">
               <Tag />
             </Route>
             <Route path="/money">
